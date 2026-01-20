1a31acaa0ae6791ebf94537d9df8244753aa63a6
snlcom <snlcom@snlcomui-MacBookPro.local>
2026-01-20 11:14:06 +0900

    Initial commit

---
+++ src/main/java/com/catchsolmind/cheongyeonbe/CheongyeonBeApplication.java
@@ -0,0 +1,13 @@
+package com.catchsolmind.cheongyeonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheongyeonBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheongyeonBeApplication.class, args);
+    }
+
+}
