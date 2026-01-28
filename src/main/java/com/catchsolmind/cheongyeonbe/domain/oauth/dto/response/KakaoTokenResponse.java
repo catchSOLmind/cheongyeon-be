f974968786457103d2118f2538470d739f193d3a
joongwon <anjoongwon517@gmail.com>
2026-01-28 16:38:02 +0900

    [Feat] 카카오토큰 관련 DTO

---
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/oauth/dto/response/KakaoTokenResponse.java
@@ -0,0 +1,24 @@
+package com.catchsolmind.cheongyeonbe.domain.oauth.dto.response;
+
+/*
+ * - 참고: https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api
+ * - 카카오 API 호출용 토큰 정보
+ * - 카카오 JSON 키와 필드명이 동일해야 함
+ */
+
+import lombok.Builder;
+
+@Builder
+public record KakaoTokenResponse(
+
+        String token_type,
+
+        String access_token,
+
+        Integer expires_in,
+
+        String refresh_token,
+
+        Integer refresh_token_expires_in
+) {
+}
