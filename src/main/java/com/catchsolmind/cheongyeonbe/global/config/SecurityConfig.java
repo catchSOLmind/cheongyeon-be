47413b99c2615bc0849fb555bee1f2974d70f92c
sijin1017 <trsj1017@naver.com>
2026-01-28 21:23:31 +0900

    [Fix] 프론트 주소 security 에 추가

--- src/main/java/com/catchsolmind/cheongyeonbe/global/config/SecurityConfig.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/global/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
         // 프론트 주소들
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://127.0.0.1:5173"
+                "http://127.0.0.1:5173",
+                "https://d288u0ybsqpwmr.cloudfront.net"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
