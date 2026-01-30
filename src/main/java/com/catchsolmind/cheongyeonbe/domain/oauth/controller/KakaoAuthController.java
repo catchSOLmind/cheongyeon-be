6ec6c1036fb735346c01b7a05d804a97fd81028e
joongwonAn <165457428+joongwonAn@users.noreply.github.com>
2026-01-30 15:25:43 +0900

    Update KakaoAuthController.java

--- src/main/java/com/catchsolmind/cheongyeonbe/domain/oauth/controller/KakaoAuthController.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/oauth/controller/KakaoAuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/oauth/kakao")
+@RequestMapping("/api/oauth/kakao")
 @Validated
 @Slf4j
 public class KakaoAuthController {
