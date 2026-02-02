a698767e34585fb95009b2357644b686dc603e82
joongwon <anjoongwon517@gmail.com>
2026-02-03 00:05:18 +0900

    [Feat] 가사성향테스트 질문 목록 조회 컨트롤러

--- src/main/java/com/catchsolmind/cheongyeonbe/domain/houseworktest/controller/HouseworkTestController.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/houseworktest/controller/HouseworkTestController.java
@@ -6,17 +6,20 @@ import com.catchsolmind.cheongyeonbe.domain.houseworktest.dto.response.Housework
 import com.catchsolmind.cheongyeonbe.domain.houseworktest.service.HouseworkTestService;
 import com.catchsolmind.cheongyeonbe.global.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/housework-test")
+@Slf4j
 public class HouseworkTestController implements HouseworkTestApi {
     private final HouseworkTestService houseworkTestService;
 
     @Override
     @GetMapping("/questions")
     public ApiResponse<HouseworkTestQuestionsResponse> questions() {
+        log.info("컨트롤러 진입");
         return ApiResponse.success(houseworkTestService.getQuestions());
     }
 
