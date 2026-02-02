5d048e1cc9330558d41e73d3e63fc5904c2bd002
joongwon <anjoongwon517@gmail.com>
2026-02-02 23:17:56 +0900

    [Fix] 테이블 생성이 완료된 후 데이터 스크립트 실행하도록 수정

---
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/houseworktest/service/HouseworkTestService.java
@@ -0,0 +1,12 @@
+package com.catchsolmind.cheongyeonbe.domain.houseworktest.service;
+
+import com.catchsolmind.cheongyeonbe.domain.houseworktest.dto.response.HouseworkTestQuestionsResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HouseworkTestService {
+    public HouseworkTestQuestionsResponse getQuestions() {
+
+        return null;
+    }
+}
