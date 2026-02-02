3389ad116b62f997c266da8e63dc39860e38b512
sijin1017 <trsj1017@naver.com>
2026-02-02 14:53:58 +0900

    [Feat] PostponeReasonCode 추가

---
+++ src/main/java/com/catchsolmind/cheongyeonbe/global/enums/PostponeReasonCode.java
@@ -0,0 +1,12 @@
+package com.catchsolmind.cheongyeonbe.global.enums;
+
+public enum PostponeReasonCode {
+    LACK_OF_TIME,      // 시간이 부족해요
+    OTHER_SCHEDULE,    // 다른 일정이 생겼어요
+    SICK,              // 몸이 안 좋아요
+    NO_TOOL,           // 청소 도구가 없어요
+    FORGOT,            // 깜빡했어요
+    NOT_HOME,          // 집에 없어요
+    ETC                // 기타 (텍스트 필수)
+}
+
