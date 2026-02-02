326eb5bc69f043f194aaed6c31bbbed7b5bee1e0
joongwon <anjoongwon517@gmail.com>
2026-02-03 00:05:06 +0900

    [Feat] 가사성향테스트 질문 목록 조회 서비스 로직

--- src/main/java/com/catchsolmind/cheongyeonbe/domain/houseworktest/service/HouseworkTestService.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/houseworktest/service/HouseworkTestService.java
@@ -1,12 +1,48 @@
 package com.catchsolmind.cheongyeonbe.domain.houseworktest.service;
 
+import com.catchsolmind.cheongyeonbe.domain.houseworktest.dto.response.HouseworkTestQuestionResponse;
 import com.catchsolmind.cheongyeonbe.domain.houseworktest.dto.response.HouseworkTestQuestionsResponse;
+import com.catchsolmind.cheongyeonbe.domain.houseworktest.entity.HouseworkTestChoice;
+import com.catchsolmind.cheongyeonbe.domain.houseworktest.entity.HouseworkTestQuestion;
+import com.catchsolmind.cheongyeonbe.domain.houseworktest.repository.HouseworkTestChoiceRepository;
+import com.catchsolmind.cheongyeonbe.domain.houseworktest.repository.HouseworkTestQuestionRepository;
+import com.catchsolmind.cheongyeonbe.global.BusinessException;
+import com.catchsolmind.cheongyeonbe.global.ErrorCode;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Service
-public class HouseworkTestService {
-    public HouseworkTestQuestionsResponse getQuestions() {
+import java.util.List;
 
-        return null;
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class HouseworkTestService {
+    private final HouseworkTestQuestionRepository questionRepository;
+    private final HouseworkTestChoiceRepository choiceRepository;
+
+    public HouseworkTestQuestionsResponse getQuestions() {
+        log.info("서비스 코드 진입");
+        List<HouseworkTestQuestion> questions = questionRepository.findAllByOrderByQuestionOrderAsc();
+        if (questions.isEmpty()) {
+            throw new BusinessException(ErrorCode.INVALID_CHOICE);
+        }
+
+        List<HouseworkTestQuestionResponse> responses =
+                questions.stream()
+                        .map(question -> {
+                            List<HouseworkTestChoice> choices =
+                                    choiceRepository.findAllByQuestion(question);
+
+                            if (choices.isEmpty()) {
+                                throw new BusinessException(ErrorCode.QUESTION_NOT_FOUND);
+                            }
+
+                            return HouseworkTestQuestionResponse.from(question, choices);
+                        })
+                        .toList();
+        return HouseworkTestQuestionsResponse.of(responses);
     }
 }
