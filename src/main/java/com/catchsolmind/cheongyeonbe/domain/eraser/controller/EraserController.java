500fa521c5530f57f3d3ed939defbac2a8d01392
joongwon <anjoongwon517@gmail.com>
2026-02-04 01:29:04 +0900

    [Feat] 청연 지우개의 업무 목록 조회와 옵션 선택 API

---
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/eraser/controller/EraserController.java
@@ -0,0 +1,47 @@
+package com.catchsolmind.cheongyeonbe.domain.eraser.controller;
+
+import com.catchsolmind.cheongyeonbe.domain.eraser.dto.response.EraserTaskOptionsResponse;
+import com.catchsolmind.cheongyeonbe.domain.eraser.dto.response.RecommendationResponse;
+import com.catchsolmind.cheongyeonbe.global.ApiResponse;
+import com.catchsolmind.cheongyeonbe.global.security.jwt.JwtUserDetails;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/eraser")
+@Slf4j
+public class EraserController implements EraserApi {
+
+    @Override
+    @GetMapping("/recommendations")
+    public ApiResponse<List<RecommendationResponse>> getRecommendations(
+            @AuthenticationPrincipal JwtUserDetails principal
+    ) {
+        return null;
+    }
+
+    @Override
+    @GetMapping("/options")
+    public ApiResponse<List<EraserTaskOptionsResponse>> getTaskOptions(
+            @RequestParam List<Long> taskIds
+    ) {
+        return null;
+    }
+
+//    @Override
+//    @PostMapping("/reservation")
+//    public ApiResponse<?> makeReservation() {
+//        return null;
+//    }
+//
+//    @Override
+//    @PostMapping("/complete-reservation")
+//    public ApiResponse<?> completeReservation() {
+//        return null;
+//    }
+}
