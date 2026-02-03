8b797cabe92624a34e05ceb2b6f120b48ae97fdb
sijin1017 <trsj1017@naver.com>
2026-02-03 20:49:59 +0900

    [Feat] 가사업무(공유) API 구현

--- src/main/java/com/catchsolmind/cheongyeonbe/domain/group/repository/GroupMemberRepository.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/group/repository/GroupMemberRepository.java
@@ -1,8 +1,10 @@
 package com.catchsolmind.cheongyeonbe.domain.group.repository;
 
 import com.catchsolmind.cheongyeonbe.domain.group.entity.GroupMember;
+import com.catchsolmind.cheongyeonbe.global.enums.MemberStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
@@ -10,4 +12,10 @@ public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> 
     Optional<GroupMember> findByGroup_GroupIdAndUser_UserId(Long groupId, Long userId);
 
     boolean existsByGroup_GroupIdAndUser_UserId(Long groupId, Long userId);
+
+    // LEFT 상태 제외 멤버 목록
+    List<GroupMember> findByGroup_GroupIdAndStatusNot(Long groupId, MemberStatus status);
+
+    // 그룹 전체 멤버 목록
+    List<GroupMember> findByGroup_GroupId(Long groupId);
 }
