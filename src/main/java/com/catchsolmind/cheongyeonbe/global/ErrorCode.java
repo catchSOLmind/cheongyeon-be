2dc0b77d96d8dc9137d9af8849d034ef7abe7d52
joongwon <anjoongwon517@gmail.com>
2026-02-04 05:15:29 +0900

    [Feat] 에러코드 추가

--- src/main/java/com/catchsolmind/cheongyeonbe/global/ErrorCode.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/global/ErrorCode.java
@@ -10,6 +10,9 @@ public enum ErrorCode {
     USER_NOT_FOUND("USER001", "사용자를 찾을 수 없습니다."),
     INVALID_HOUSEWORK_TYPE("USER002", "유효하지 않은 가사 성향 타입입니다."),
 
+    // Group
+    GROUP_NOT_FOUND("GROUP001", "그룹을 찾을 수 없습니다."),
+
     // File
     INVALID_FILE_TYPE("FILE001", "허용되지 않는 파일 형식입니다."),
     FILE_SIZE_EXCEEDED("FILE002", "파일 크기가 제한을 초과했습니다."),
