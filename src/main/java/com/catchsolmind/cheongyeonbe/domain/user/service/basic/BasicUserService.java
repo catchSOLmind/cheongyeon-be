8c79c0119fe4ac4223586f27d5ff08cef637d187
joongwon <anjoongwon517@gmail.com>
2026-02-03 17:14:15 +0900

    [Fix] @Transactional 추가 및 에러코드 통일

--- src/main/java/com/catchsolmind/cheongyeonbe/domain/user/service/basic/BasicUserService.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/user/service/basic/BasicUserService.java
@@ -6,11 +6,14 @@ import com.catchsolmind.cheongyeonbe.domain.user.entity.User;
 import com.catchsolmind.cheongyeonbe.domain.user.mapper.UserMapper;
 import com.catchsolmind.cheongyeonbe.domain.user.repository.UserRepository;
 import com.catchsolmind.cheongyeonbe.domain.user.service.UserService;
-import com.catchsolmind.cheongyeonbe.global.enums.AuthProvider;
+import com.catchsolmind.cheongyeonbe.global.BusinessException;
+import com.catchsolmind.cheongyeonbe.global.ErrorCode;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class BasicUserService implements UserService {
     private final UserRepository userRepository;
@@ -18,8 +21,9 @@ public class BasicUserService implements UserService {
 
     @Override
     public UserDto findOrCreate(OAuthUserInfo info) {
+
         return userRepository
-                .findByProviderAndProviderId(AuthProvider.KAKAO, info.providerId())
+                .findByProviderAndProviderId(info.provider(), info.providerId())
                 .map(userMapper::toDto)
                 .orElseGet(() -> createUser(info));
     }
@@ -27,12 +31,13 @@ public class BasicUserService implements UserService {
     @Override
     public User findEntityById(Long userId) {
         return userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
     }
 
     private UserDto createUser(OAuthUserInfo info) {
         User user = User.createOAuthUser(info);
+        User savedUser = userRepository.save(user);
 
-        return userMapper.toDto(userRepository.save(user));
+        return userMapper.toDto(savedUser);
     }
 }
