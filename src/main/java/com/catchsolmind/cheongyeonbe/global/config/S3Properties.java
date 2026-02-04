5073cd54fe7324c3a83395817d1bb8ce71400e4d
joongwon <anjoongwon517@gmail.com>
2026-02-04 14:16:00 +0900

    [Chore] s3의 base-url 설정

---
+++ src/main/java/com/catchsolmind/cheongyeonbe/global/config/S3Properties.java
@@ -0,0 +1,16 @@
+package com.catchsolmind.cheongyeonbe.global.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "app.s3")
+public class S3Properties {
+    private String bucket;
+    private String region;
+    private String baseUrl;
+}
