78a908fc1e767c7bb981a452bba451f340f81d7f
sijin1017 <trsj1017@naver.com>
2026-02-04 14:15:40 +0900

    [Fix] Security Config 수정

--- src/main/java/com/catchsolmind/cheongyeonbe/global/config/SwaggerConfig.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/global/config/SwaggerConfig.java
@@ -3,12 +3,14 @@ package com.catchsolmind.cheongyeonbe.global.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "Cheongyeon API", version = "v1")
+        info = @Info(title = "Cheongyeon API", version = "v1"),
+        security = @SecurityRequirement(name = "bearerAuth")
 )
 @SecurityScheme(
         name = "bearerAuth",
