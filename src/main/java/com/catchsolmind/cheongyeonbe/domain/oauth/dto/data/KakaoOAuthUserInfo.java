788214aad785c37d976d4a79a89d55611ab2ad46
joongwon <anjoongwon517@gmail.com>
2026-02-06 00:12:06 +0900

    [Feat] 카카오 기본 이미지 여부 필드

--- src/main/java/com/catchsolmind/cheongyeonbe/domain/oauth/dto/data/KakaoOAuthUserInfo.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/oauth/dto/data/KakaoOAuthUserInfo.java
@@ -3,8 +3,11 @@ package com.catchsolmind.cheongyeonbe.domain.oauth.dto.data;
 import com.catchsolmind.cheongyeonbe.domain.oauth.dto.response.KakaoUserResponse;
 import com.catchsolmind.cheongyeonbe.global.BusinessException;
 import com.catchsolmind.cheongyeonbe.global.ErrorCode;
+import com.catchsolmind.cheongyeonbe.global.config.S3Properties;
 import com.catchsolmind.cheongyeonbe.global.enums.AuthProvider;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 /*
  * 카카오 전용 User Info
@@ -13,20 +16,40 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class KakaoOAuthUserInfo {
     private final KakaoUserResponse response;
+    private final S3Properties s3Properties;
+
+    private static final String DEFAULT_PROFILE_PATH = "assets/default-profile.png";
 
     public OAuthUserInfo toOAuthUserInfo() {
-        if (response == null ||
-                response.kakaoAccount() == null ||
-                response.kakaoAccount().profile() == null) {
+        if (response == null || response.kakaoAccount() == null) {
             throw new BusinessException(ErrorCode.KAKAO_SERVER_ERROR);
+        }
+
+        String serviceDefaultImageUrl = Optional.ofNullable(s3Properties.getBaseUrl())
+                .map(base -> base + "/" + DEFAULT_PROFILE_PATH)
+                .orElseThrow(() -> new BusinessException(ErrorCode.S3_CONFIG_ERROR));
+
+        KakaoUserResponse.KakaoAccount.Profile profile = response.kakaoAccount().profile();
+
+        String nickname = "GUEST";
+        String finalProfileImg = serviceDefaultImageUrl; // 일단 기본 이미지로 설정
+
+        if (profile != null) {
+            if (profile.nickname() != null) {
+                nickname = profile.nickname();
+            }
+
+            if (profile.profileImageUrl() != null && !Boolean.TRUE.equals(profile.isDefaultImage())) {
+                finalProfileImg = profile.profileImageUrl();
+            }
         }
 
         return new OAuthUserInfo(
                 AuthProvider.KAKAO,
                 response.id(),
                 response.kakaoAccount().email(),
-                response.kakaoAccount().profile().nickname(),
-                response.kakaoAccount().profile().profileImageUrl()
+                nickname,
+                finalProfileImg // 최종 결정된 이미지 URL
         );
     }
 }
