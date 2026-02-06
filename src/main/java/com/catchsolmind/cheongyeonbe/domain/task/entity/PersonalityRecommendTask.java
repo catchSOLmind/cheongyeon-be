fc3951ff0b446d4b6bf32c30665c2e4fbed551b1
sijin1017 <trsj1017@naver.com>
2026-02-06 16:35:43 +0900

    [Feat] 성향과 task_type 매핑

---
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/task/entity/PersonalityRecommendTask.java
@@ -0,0 +1,28 @@
+package com.catchsolmind.cheongyeonbe.domain.task.entity;
+
+import com.catchsolmind.cheongyeonbe.global.enums.TestResultType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "personality_recommend_task",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"personality_type", "task_type_id"}))
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PersonalityRecommendTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "personality_type", nullable = false, length = 30)
+    private TestResultType personalityType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_type_id", nullable = false)
+    private TaskType taskType;
+}
