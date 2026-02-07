8ac54257f95dbf8275c454c60e2ac0cf7e16fe0e
joongwon <anjoongwon517@gmail.com>
2026-02-07 10:46:44 +0900

    [Feat] 유저 리스트에 포함된 결과들을 한 번에 조회

--- src/main/java/com/catchsolmind/cheongyeonbe/domain/houseworktest/repository/HouseworkTestResultRepository.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/houseworktest/repository/HouseworkTestResultRepository.java
@@ -4,8 +4,12 @@ import com.catchsolmind.cheongyeonbe.domain.houseworktest.entity.HouseworkTestRe
 import com.catchsolmind.cheongyeonbe.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HouseworkTestResultRepository extends JpaRepository<HouseworkTestResult, Long> {
     Optional<HouseworkTestResult> findByUser(User user);
+
+    // 유저 리스트에 포함된 결과들을 한 번에 조회
+    List<HouseworkTestResult> findByUser_UserIdIn(List<Long> userIds);
 }
