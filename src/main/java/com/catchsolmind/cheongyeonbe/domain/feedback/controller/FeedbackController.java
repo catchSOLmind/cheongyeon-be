057a1ae9f79e08d379d137dcfb39d886a87ad668
joongwon <anjoongwon517@gmail.com>
2026-02-07 10:40:27 +0900

    [Feat] @AuthenticationPrincipal 추가

--- src/main/java/com/catchsolmind/cheongyeonbe/domain/feedback/controller/FeedbackController.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/feedback/controller/FeedbackController.java
@@ -2,9 +2,11 @@ package com.catchsolmind.cheongyeonbe.domain.feedback.controller;
 
 import com.catchsolmind.cheongyeonbe.domain.feedback.dto.request.FeedbackCreateRequest;
 import com.catchsolmind.cheongyeonbe.domain.feedback.dto.response.FeedbackResponse;
+import com.catchsolmind.cheongyeonbe.domain.feedback.service.FeedbackService;
 import com.catchsolmind.cheongyeonbe.global.ApiResponse;
 import com.catchsolmind.cheongyeonbe.global.security.jwt.JwtUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/feedback")
 public class FeedbackController implements FeedbackApi {
+    private final FeedbackService feedbackService;
 
     @Override
     @GetMapping
-    public ApiResponse<FeedbackResponse> getFeedback(JwtUserDetails principal) {
-        return null;
+    public ApiResponse<FeedbackResponse> getFeedback(@AuthenticationPrincipal JwtUserDetails principal) {
+
+        FeedbackResponse response = feedbackService.getFeedback(principal.user().getUserId());
+
+        return ApiResponse.success(response);
     }
 
     @Override
     @PostMapping
-    public ApiResponse<Void> postFeedback(JwtUserDetails principal, FeedbackCreateRequest request) {
+    public ApiResponse<Void> postFeedback(@AuthenticationPrincipal JwtUserDetails principal,
+                                          FeedbackCreateRequest request) {
         return null;
     }
 }
