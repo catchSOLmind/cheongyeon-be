761a1e1a6ff0bc28c9a924444ef9a8268f78991c
joongwon <anjoongwon517@gmail.com>
2026-02-07 10:57:52 +0900

    [Chore] 오탈자

--- src/main/java/com/catchsolmind/cheongyeonbe/domain/feedback/controller/FeedbackApi.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/feedback/controller/FeedbackApi.java
@@ -15,7 +15,7 @@ public interface FeedbackApi {
     @Operation(
             summary = "피드백 작성",
             description = "피드백 작성 페이지를 로드한다.\n\n" +
-                    "성향테스트 결과를 없을 경우 NULL을 반환한다.\n\n" +
+                    "성향테스트 결과가 없을 경우 NULL을 반환한다.\n\n" +
                     "칭찬 스탬프: \n\n" +
                     "- DETAIL_KING: 꼼꼼왕\n\n" +
                     "- TIME_KEEPER: 시간 엄수\n\n" +
