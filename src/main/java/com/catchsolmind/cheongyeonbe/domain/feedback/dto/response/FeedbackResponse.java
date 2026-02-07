4db761120423d8c028a3dcce61753a7bf0750041
joongwon <anjoongwon517@gmail.com>
2026-02-07 10:40:49 +0900

    [Feat] FE에게 칭찬 스탬프 정보 반환

--- src/main/java/com/catchsolmind/cheongyeonbe/domain/feedback/dto/response/FeedbackResponse.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/feedback/dto/response/FeedbackResponse.java
@@ -1,15 +1,18 @@
 package com.catchsolmind.cheongyeonbe.domain.feedback.dto.response;
 
-import com.catchsolmind.cheongyeonbe.global.enums.PraiseType;
 import com.catchsolmind.cheongyeonbe.global.enums.TaskCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record FeedbackResponse(
-        List<GroupMemberWithTestResponse> groupMembers,
+        @Schema(description = "그룹 멤버 List")
+        List<GroupMemberWithTestResult> groupMembers,
 
-        List<PraiseType> praiseTypes,
+        @Schema(description = "칭찬 스탬프")
+        List<PraiseTypeResponse> praiseTypes,
 
+        @Schema(description = "카테고리")
         List<TaskCategory> taskCategories
 ) {
 }
