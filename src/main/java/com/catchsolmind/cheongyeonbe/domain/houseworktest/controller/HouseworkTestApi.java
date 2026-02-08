2daa164d742910b722bb0d979abbfa30f90d5dba
joongwon <anjoongwon517@gmail.com>
2026-02-08 14:05:18 +0900

    [Chore] 톤 수정

--- src/main/java/com/catchsolmind/cheongyeonbe/domain/houseworktest/controller/HouseworkTestApi.java
+++ src/main/java/com/catchsolmind/cheongyeonbe/domain/houseworktest/controller/HouseworkTestApi.java
@@ -20,7 +20,7 @@ public interface HouseworkTestApi {
     @Operation(
             summary = "가사 성향 테스트 제출 및 결과 생성",
             description = "사용자의 답변을 제출하고 가사 성향 테스트 결과를 계산하여 반환\n\n" +
-                    "점수는 꼼꼼함, 착실함, 유지력, 대충력 순서로 줬음"
+                    "점수는 꼼꼼함, 착실함, 유지력, 대충력 순서로 반환됩니다."
     )
     ApiResponse<HouseworkTestResultResponse> submitResult(
             HouseworkTestSubmitRequest request,
